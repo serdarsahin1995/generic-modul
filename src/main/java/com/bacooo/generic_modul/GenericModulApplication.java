0d41cf88562991415d8ea795653ff36df23f3143
Serdar <serdar.sahin.9534@gmail.com>
2025-08-30 18:06:34 +0300

    Deneme3

--- src/main/java/com/bacooo/generic_modul/GenericModulApplication.java
+++ src/main/java/com/bacooo/generic_modul/GenericModulApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GenericModulApplication {
 
 	public static void main(String[] args) {
+        System.out.println("sd");
 		SpringApplication.run(GenericModulApplication.class, args);
 	}
 
